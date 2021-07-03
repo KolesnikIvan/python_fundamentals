9d8a300be6e689a37799ca9ab337aa47b8144e23
KolesnikIvan <86139761+KolesnikIvan@users.noreply.github.com>
2021-07-04 02:23:32 +0600

    Delete utils.py

--- Kolesnik_Ivan_dz_4/utils.py
+++
@@ -1,7 +0,0 @@
-import task_4_2 as t42
-
-print(*t42.currency_rates('gbp'), sep=', ')
-print(*t42.currency_rates('kzt'), sep=', ')
-print(*t42.currency_rates('bgn'), sep=', ')
-print(*t42.currency_rates('cad'), sep=', ')
-print(*t42.currency_rates('foo'), sep=', ')
